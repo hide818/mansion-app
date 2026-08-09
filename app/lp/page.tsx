85d09ff0a9273cec01bde92d19e6c9108023e27e
小松　秀彰 <h.komatsu@komatsuhideakinoMacBook-Air.local>
2026-08-10 00:20:05 +0900

    remove video controls to prevent unmute

--- app/lp/page.tsx
+++ app/lp/page.tsx
@@ -306,7 +306,6 @@ export default function LpPage() {
               autoPlay
               muted
               loop
-              controls
               playsInline
               className="w-full"
               preload="auto"
